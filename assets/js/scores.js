f11b6c29b1334fb99a312ebcaa2a09d11d253d17
nfallis96 <nfallis2019@fau.edu>
2022-12-18 18:38:43 -0600

    restarted it and made a lot of changes to the project

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,32 +1,28 @@
-// variables needed
+// variables that connect html elements to js
 var scores = document.querySelector("#scores");
 var clearScores = document.querySelector("#clear-scores");
 var backButton = document.querySelector("#back-button");
 
-// add event listener to clear scores button that listens for a click and for a function to clear local storage
+// clear local storage
 clearScores.addEventListener("click", function() {
     localStorage.clear();
     location.reload();
 });
 
-// get all scores from local storage
+// gabs all scores from local storage to display
 var allScores = localStorage.getItem("allScores");
-// use JSON.parse to de-stringify all scores objects in local storage
 allScores = JSON.parse(allScores);
 
-// if all scores are not null, run for loop to repeat through all scores
+// if scores exists, then loop through them and creat an li for each score and initial set
 if (allScores !== null) {
     for (var i = 0; i < allScores.length; i++) {
-        
-        // create li for each individual score object (initials, score)
         var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " - " + allScores[i].score;
-        // attach to page
         scores.appendChild(createLi);
     }
 }
 
-// add event listener that listens for click and navigates back to the quiz, where user can retake quiz
+//go back to index.html
 backButton.addEventListener("click", function() {
     window.location.replace("./index.html");
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// starting numeric values for quiz
+var currentScore = 0;
+var questionNumber = 0;
+var startingTime = 60;
+var timerId = 0;
+var incorrectPenalty = 15;
+
+// variables to connect html elements to the js
+var createList = document.createElement("ol");
+var timeLeft = document.querySelector("#time-left");
+var startGame = document.querySelector("#start-button");
+var quizQuestion = document.querySelector("#quiz-question");
+var card = document.querySelector("#card");
+var createParagraph = document.getElementById("create-paragraph")
+var createInitials = document.getElementById("initials")
+var createSubmitButton = document.getElementById("submit-button");
+var startquiz = document.querySelector(".startQuiz")
+var endquiz = document.querySelector(".endQuiz")
+endquiz.style.display = "none";
+
+// array of questions, choices, and correct answer
 var questions = [
     {
         question: "Javascript is an _________ language.",
@@ -26,46 +47,12 @@ var questions = [
     },
 ];
 
-// create needed variables:
-
-// begin with score at 0
-var currentScore = 0; 
-
-// beginning of the array of questions 
-var questionNumber = 0;
-
-// time left on the timer
-var timeLeft = document.querySelector("#time-left");
-
-// the start button for the quiz and timer
-var startGame = document.querySelector("#start-button");
-
-// where the questions will be displayed
-var quizQuestion = document.querySelector("#quiz-question");
-
-// place where questions, choices, and start button will be held
-var card = document.querySelector("#card");
-
-// time for the quiz
-var startingTime = 60;
-
-// Interval declared for use in functions
-var timerId = 0;
-
-// penalty for incorrect answer
-var incorrectPenalty = 15;
-
-// create ul elements
-var createList = document.createElement("ol");
-
-
-// start timer on click and show timer on screen
-startGame.addEventListener("click", function() {
+// when you click start button, the timer starts and we call teh render questions function
+startGame.addEventListener("click", function () {
     if (timerId === 0) {
-        timerId = setInterval(function() {
+        timerId = setInterval(function () {
             startingTime--;
             timeLeft.textContent = "Seconds Left: " + startingTime;
-
             if (startingTime <= 0) {
                 clearInterval(timerId);
                 endGame();
@@ -76,135 +63,75 @@ startGame.addEventListener("click", function() {
     render(questionNumber);
 });
 
-// give question and choices 
+// this function shows each question, and choices for each question
 function render(questionNumber) {
-    questionNumber.innerHTML = "";
     createList.innerHTML = "";
-    
-    for (var i = 0; i < questions.length; i++) {
-        var displayQuestion = questions[questionNumber].question;
-        var displayChoices = questions[questionNumber].choices;
-        quizQuestion.textContent = displayQuestion;
-    }
-    // use to display choices
-    displayChoices.forEach(function (newEl) {
+    var displayQuestion = questions[questionNumber].question;
+    var displayChoices = questions[questionNumber].choices;
+    quizQuestion.textContent = displayQuestion;
+    // loops through each choice and creates an li for it
+    displayChoices.forEach(function (i) {
         var li = document.createElement("li");
-        li.textContent = newEl;
+        li.textContent = i;
         quizQuestion.appendChild(createList);
         createList.appendChild(li);
         li.addEventListener("click", (compareAnswer));
     })
 }
 
-// if correct answer display that it is correct. if wrong display that it is wrong
-// if last question, end the game and display message telling user how many answers they got correct
+// this function compars what the user clicked on, to what questions[i].answer is to check if what they clicked, is wrong or right
 function compareAnswer(e) {
     var selection = e.target;
-    
     if (selection.matches("li")) {
-        var createDiv = document.createElement("div");
-        createDiv.setAttribute("id", "createDiv");
+        // if clicked on is correct, then they got it right
         if (selection.textContent == questions[questionNumber].answer) {
             currentScore++;
-            createDiv.textContent = questions[questionNumber].answer + " is correct!";
+            // if clicked on not correctm then they got it wrong
         } else {
             startingTime = startingTime - incorrectPenalty;
-            createDiv.textContent = "That is incorrect, the answer is: " + questions[questionNumber].answer;
         }
     }
+    // after checking if clicked on is right or wrong, it goes to the next question and set of answers
     questionNumber++;
-
+    // becuase im not using a for loop, i need to tell it when the end of the questions array is hit to end the game
     if (questionNumber >= questions.length) {
-        createDiv.textContent = "Game Over! You correctly answered " + currentScore + "/" + questions.length + " questions";
         endGame();
     } else {
         render(questionNumber);
     }
-    quizQuestion.appendChild(createDiv);
 }
 
-// end game function 
+// this function will end the quiz, and prompt them to enter intials
 function endGame() {
-    // clear content from quizQuestion and timeLeft
+    startquiz.style.display = "none"
+    endquiz.style.display = "block"
     quizQuestion.innerHTML = "";
     timeLeft.innerHTML = "";
     var timeRemaining = 0;
-
-    // create Heading that says Quiz Complete and attach to page
-    var createHeading = document.createElement("h1");
-    createHeading.setAttribute("id", "create-heading");
-    createHeading.textContent = "Quiz Complete!";
-
-    quizQuestion.appendChild(createHeading);
-
-    // to display final score
-    var createParagraph = document.createElement("p");
-    createParagraph.setAttribute("id", "create-paragraph");
-
-    quizQuestion.appendChild(createParagraph);
-
-    // set score equal to time left on the timer
     if (startingTime >= 0) {
         timeRemaining = startingTime;
         clearInterval(timerId);
     }
-
-    createParagraph.textContent = "Final Score: " + timeRemaining;
-
-    // create label for user to enter initials and attach to page
-    var createLabel = document.createElement("label");
-    createLabel.setAttribute("id", "create-label");
-    createLabel.textContent = "Enter your initials: ";
-
-    quizQuestion.appendChild(createLabel);
-
-    // create input for user to enter initials and attach to page
-    var createInput = document.createElement("input");
-    createInput.setAttribute("id", "initials");
-    createInput.setAttribute("type", "text");
-    createInput.textContent = "";
-
-    quizQuestion.appendChild(createInput);
-
-    // create submit button and attach to page
-    var createSubmitButton = document.createElement("button");
-    createSubmitButton.setAttribute("id", "submit-button");
-    createSubmitButton.setAttribute("type", "submit");
-    createSubmitButton.textContent = "Submit";
-
-    quizQuestion.appendChild(createSubmitButton);
-
-    // add event listener to submit button so that it responds to click.
-    createSubmitButton.addEventListener("click", function() {
-        var initials = createInput.value;
-        if(initials === null) {
-            console.log("No initials entered"); 
+    createParagraph.textContent = timeRemaining;
+    createSubmitButton.addEventListener("click", function () {
+        var initials = createInitials.value;
+        if (initials === null || initials == "") {
+            console.log("No initials entered");
         } else {
-                var endScore = {
+            var endScore = {
                 initials: initials,
                 score: timeRemaining
             }
-            
-            // get all previous scores from local storage
             var allScores = localStorage.getItem("allScores");
-            if(allScores === null) {
+            if (allScores === null) {
                 allScores = [];
             } else {
-                // user JSON.parse to de-stringify scores when pulling out of local storage
                 allScores = JSON.parse(allScores);
             }
-            // push end score of the quiz to all scores in local storage
             allScores.push(endScore);
-            
-            // use JSON.stringify to add newAllScores to local storage
             var newAllScores = JSON.stringify(allScores);
             localStorage.setItem("allScores", newAllScores);
-
-            // navigate to leaderboard page
-            window.location.replace("./highscores.html");
+            window.location.replace("scores.html");
         }
     });
-
 }
-
- 
